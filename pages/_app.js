5fa93ebff35dd1128985cab663955d10fb64f668
Sahitya77 <sahityanijhawan@gmail.com>
2023-02-05 10:46:31 +0530

    minor updates

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>dekode</title>
+        <title>Keybrief</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
